3f920010a72c4c08c1b6440fe06bfd2543edf6d3
pdiakumis <pdiakumis@unimelb.edu.au>
2018-05-14 15:09:06 +1000

    produce sample yaml for A5 batch 2

--- scripts/df2yaml.R
+++ scripts/df2yaml.R
@@ -33,6 +33,8 @@ get_new_batch <- function(batch, type) {
   return(new_batch)
 }
 
+#---- s1 ----#
+
 s1 %>%
   as_tibble() %>%
   separate(value, c("batch", "phenotype", "d"), "-", remove = FALSE) %>%
@@ -50,7 +52,6 @@ s1 %>%
   arrange(new_batch, type) -> s1
 
 # For each folder, get the BAM and VCF file
-# We can just ignore the normal VCF file
 (f1 <- s1$folder[1])
 list.files(file.path(a5_1, f1), pattern = "bam$")
 list.files(file.path(a5_1, f1), pattern = "sv-prioritize-manta.vcf.gz$")
@@ -91,3 +92,61 @@ names(sample_list) <- s1$new_batch
 sample_list <- list(samples = sample_list)
 cat(yaml::as.yaml(sample_list)) # awesome
 write(as.yaml(sample_list), file = "../workflows/structural/samples_A5_batch1.yaml")
+
+#---- s2 ----#
+
+s2 %>%
+  as_tibble() %>%
+  separate(value, c("batch", "phenotype", "d"), "-", remove = FALSE) %>%
+  separate(batch, c("pr", "batch"), "_") %>%
+  mutate(type = case_when(
+    grepl("^B", phenotype) ~ "normal",
+    grepl("^T", phenotype) ~ "tumor",
+    TRUE ~ phenotype)) %>%
+  rename(folder = value) %>%
+  select(folder, batch, phenotype, type) %>%
+  group_by(batch) %>%
+  mutate(new_batch = get_new_batch(batch, type)) %>%
+  cSplit(splitCols = "new_batch", sep = ";", direction = "long", type.convert = FALSE) %>%
+  as_tibble() %>%
+  arrange(new_batch, type) -> s2
+
+# For each folder, get the BAM and VCF file
+
+bams <- vector("character", length = nrow(s2))
+vcfs <- vector("character", length = nrow(s2))
+
+for (i in seq_len(nrow(s2))) {
+  bams[i] <- list.files(file.path(a5_2, s2$folder[i]), pattern = "ready.bam$")
+  vcfs[i] <- list.files(file.path(a5_2, s2$folder[i]), pattern = "sv-prioritize-manta.vcf.gz$")
+}
+
+s2 <- s2 %>%
+  mutate(bam = bams,
+         vcf = vcfs) %>%
+  mutate(bam = paste(folder, bam, sep = "/"),
+         vcf = paste(folder, vcf, sep = "/")) %>%
+  select(new_batch, type, bam, vcf) %>%
+  gather(file_type, path, -c(new_batch, type)) %>%
+  unite(temp, type, file_type) %>%
+  spread(temp, path)
+
+sample_list <- vector(mode = "list", length = nrow(s2))
+
+
+for (i in 1:nrow(s2)) {
+  sample_list[[i]] <- list(
+    normal = list(
+      bam = s2$normal_bam[i],
+      vcf = s2$normal_vcf[i]),
+    tumor = list(
+      bam = s2$tumor_bam[i],
+      vcf = s2$tumor_vcf[i])
+  )
+}
+names(sample_list) <- s2$new_batch
+
+sample_list <- list(samples_A5_batch2 = sample_list)
+cat(yaml::as.yaml(sample_list)) # awesome
+write(as.yaml(sample_list), file = "../workflows/structural/samples_A5_batch2.yaml")
+
