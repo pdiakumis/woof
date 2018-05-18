a34cb39d1b6183a5b606c7c8c42988923a406020
pdiakumis <pdiakumis@unimelb.edu.au>
2018-05-18 16:49:32 +1000

    write other way around..

--- scripts/structural/facets_fit2tsv.R
+++ scripts/structural/facets_fit2tsv.R
@@ -11,9 +11,9 @@ optlist <- list(
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list = optlist))
 # for debugging
-opt <- list(
-  facets = "/data/cephfs/punim0010/projects/Diakumis_woof/data/out/facets/results/A5_batch1/E131/E131_cval_150_fit.rds"
-)
+#opt <- list(
+#  facets = "/data/cephfs/punim0010/projects/Diakumis_woof/data/out/facets/results/A5_batch1/E131/E131_cval_150_fit.rds"
+#)
 stopifnot(file.exists(opt$facets))
 
 base_nm <- tools:::file_path_sans_ext(opt$facets)
@@ -22,8 +22,8 @@ facets_list <- readRDS(opt$facets)
 
 # export purity/ploidy
 purity_ploidy <- c(purity = facets_list[["purity"]], ploidy = facets_list[["ploidy"]])
-write.table(paste0(base_nm, "_purply.tsv"), as.data.frame(t(purity_ploidy)), row.names = FALSE, sep = "\t", quote = FALSE)
+write.table(as.data.frame(t(purity_ploidy)), paste0(base_nm, "_purply.tsv"), row.names = FALSE, sep = "\t", quote = FALSE)
 
 # export cncf
 cncf <- facets_list[["cncf"]]
-write.table(paste0(base_nm, "_segs.tsv"), cncf, row.names = FALSE, sep = "\t", quote = FALSE)
+write.table(cncf, paste0(base_nm, "_segs.tsv"), row.names = FALSE, sep = "\t", quote = FALSE)
