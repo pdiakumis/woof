0606ee3cf6bb6b93626dc2b13d33ece2ad6bdb62
pdiakumis <pdiakumis@unimelb.edu.au>
2018-08-05 15:32:08 +1000

    change sample to batch

--- scripts/structural/facets/run_facets.R
+++ scripts/structural/facets/run_facets.R
@@ -6,28 +6,28 @@ suppressPackageStartupMessages(library(facets))
 suppressPackageStartupMessages(library(optparse))
 set.seed(13)
 
-# RScript -s <sample name> -f <snp file> -c <cval> -o <output dir>
+# RScript -b <batch-name> -f <snp-file> -c <cval> -o <output-dir>
 option_list <- list(
-  make_option(c("-s", "--samplename"), action = "store", type = "character", default = NULL,
-              help = "Sample name"),
+  make_option(c("-b", "--batchname"), action = "store", type = "character", default = NULL,
+              help = "Batch name"),
   make_option(c("-f", "--snpfile"), action = "store", type = "character", default = NULL,
               help = "Path to file containing SNP allele counts"),
   make_option(c("-c", "--cval"), action = "store", type = "double", default = 150,
               help = "Critical value [%default]"),
   make_option(c("-o", "--outdir"), action = "store", type = "character", default = NULL,
-              help = "Output directory for plots and objects")
+              help = "Output directory for final plots and files")
 )
 
 opt <- parse_args(OptionParser(option_list = option_list))
 
 # For interactive run, use following template
-# opt <- list(samplename = "batch1",
-#            snpfile = "/data/cephfs/punim0010/projects/Diakumis_HCC2218/woof/final/structural/facets/pileup/batch1/HCC2218_N.HCC2218_T.pileup.csv.gz",
-#            cval = 150,
-#            outdir = "/data/cephfs/punim0010/projects/Diakumis_HCC2218/woof/final/structural/facets/results/batch1")
+opt <- list(batchname = "HCC2218",
+           snpfile = "/data/cephfs/punim0010/projects/Diakumis_HCC2218/woof/final/structural/facets/pileup/HCC2218.pileup.csv.gz",
+           cval = 150,
+           outdir = "/data/cephfs/punim0010/projects/Diakumis_HCC2218/woof/final/structural/facets/results")
 
 stopifnot(!is.null(opt$snpfile),
-          !is.null(opt$samplename),
+          !is.null(opt$batchname),
           is.numeric(opt$cval),
           !is.null(opt$outdir))
 
@@ -36,10 +36,12 @@ stopifnot(!is.null(opt$snpfile),
 cat(stamp(), "Starting Facets analysis\n")
 cat(stamp(), paste0("Arguments specified:\n",
                     "\tsnpfile: ", opt$snpfile, "\n",
-                    "\tsamplename: ", opt$samplename, "\n",
+                    "\tbatchname: ", opt$batchname, "\n",
                     "\tcval: ", opt$cval, "\n",
                     "\toutdir: ", opt$outdir, "\n"))
 
+# output will be in facets/results/batch/batch_cval_x
+opt$outdir <- file.path(opt$outdir, opt$batchname)
 if (!dir.exists(opt$outdir)) {
   dir.create(opt$outdir, recursive = TRUE)
 }
@@ -73,7 +75,7 @@ cat(stamp(), "Ploidy is:", fit$ploidy, "\n")
 #     using the EM-algorithm
 
 # Plots
-prefix <- file.path(opt$outdir, paste0(opt$samplename, "_cval_", opt$cval))
+prefix <- file.path(opt$outdir, paste0(opt$batchname, "_cval_", opt$cval))
 cnv_plotname <- paste0(prefix, "_cnv")
 spider_plotname <- paste0(prefix, "_spider")
 
@@ -95,12 +97,11 @@ png(paste0(spider_plotname, ".png"))
 logRlogORspider(proc$out, proc$dipLogR)
 dev.off()
 
-# Save Objects
+# Save files
 cat(stamp(), "Saving stuff\n")
 purity_ploidy <- c(purity = fit$purity, ploidy = fit$ploidy, dipLogR = fit$dipLogR, loglik = fit$loglik)
 write.table(as.data.frame(t(purity_ploidy)), paste0(prefix, "_purply.tsv"), row.names = FALSE, sep = "\t", quote = FALSE)
 
-# export cncf
 cncf <- fit$cncf
 write.table(cncf, paste0(prefix, "_segs.tsv"), row.names = FALSE, sep = "\t", quote = FALSE)
 cat(stamp(), "Finished Facets analysis\n")
