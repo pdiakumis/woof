60d4227b8811aacb2b1204608e850c0b7029b62f
pdiakumis <pdiakumis@unimelb.edu.au>
2018-08-07 18:06:23 +1000

    adding fasta ref

--- woof/__init__.py
+++ woof/__init__.py
@@ -21,12 +21,12 @@ hpc_dict = {
     'SPARTAN' : {
         'extras' : '/data/cephfs/punim0010/extras',
         'woof_data' : '/data/cephfs/punim0010/extras/woof/data',
-        'java' : 'Java/9.0.4',
+        'ref_fasta' : '/data/cephfs/punim0010/local/development/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa'
         },
     'RAIJIN' : {
         'extras' : '/g/data3/gx8/extras',
         'woof_data' : '/g/data3/gx8/extras/woof/data',
-        'java' : 'java/jdk1.8.0_60',
+        'ref_fasta' : '/g/data3/gx8/local/development/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa'
         }
 }
 
