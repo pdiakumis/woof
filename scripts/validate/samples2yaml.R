03e649bfdb1f845f0f10ea70d9864a03e55f3358
pdiakumis <pdiakumis@unimelb.edu.au>
2018-10-15 16:55:56 +1100

    validate: generate sample yaml given dir

--- scripts/validate/samples2yaml.R
+++ scripts/validate/samples2yaml.R
@@ -2,15 +2,17 @@ require(dplyr)
 require(fs)
 require(readr)
 require(yaml)
-require(rock, lib.loc = "~/rock/master")
+require(rock)
 
-d <- "/Users/pdiakumis/Downloads/fastq_invalid_tests" %>%
+# what files do we have in the given directory
+data_dir <- "/Users/pdiakumis/Downloads/fastq_invalid_tests"
+d <- data_dir %>%
   fs::dir_ls() %>%
   tibble::as_tibble() %>%
   purrr::set_names("abspath") %>%
   dplyr::mutate(fname = fs::path_file(abspath),
-                ftype = rock:::guess_file_type(fname)) %>%
-  dplyr::filter(ftype == "FASTQ")
+                ftype = rock:::guess_file_type(fname))
+  # dplyr::filter(ftype == "FASTQ")
 
 stopifnot(!any(duplicated(d$fname)))
 
@@ -20,9 +22,14 @@ names(l) <- d$fname
 for (i in 1:nrow(d)) {
   abspath <- d$abspath[i]
   fname <- d$fname[i]
-  mini_list <- list(abspath = abspath, fname = fname)
+  ftype <- d$ftype[i]
+
+  mini_list <- list(abspath = abspath, fname = fname, ftype = ftype)
   l[[i]] <-  mini_list
 }
+
+l <- list(l)
+names(l) <- basename(data_dir)
 
 cat(yaml::as.yaml(l))
 write(yaml::as.yaml(l), file = "../../config/validate.yaml")
