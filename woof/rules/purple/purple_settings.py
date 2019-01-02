6ae75fcbaa4f57add0fa4200749bbd8b8fec2920
pdiakumis <pdiakumis@unimelb.edu.au>
2019-01-02 13:49:53 +1100

    PURPLE: deprecate support

--- woof/rules/purple/purple_settings.py
+++
@@ -1,9 +0,0 @@
-config['tools']['purple'] = {
-    'outdir' : join(config['woof']['final_dir'], 'structural/purple'),
-    'hmf_data' : {
-        'dir' : join(config['HPC']['woof_data'], 'hmf'),
-    },
-}
-
-config['tools']['purple']['hmf_data']['gc_profile'] = join(config['tools']['purple']['hmf_data']['dir'], 'GC_profile.GRCh37.1000bp.cnp')
-config['tools']['purple']['hmf_data']['snp_bed'] = join(config['tools']['purple']['hmf_data']['dir'], 'GermlineHetPon.GRCh37.bed')
