8fcfc937a417ac93dbd85a8de45ed9c32bf3b0bc
pdiakumis <pdiakumis@unimelb.edu.au>
2019-03-05 23:22:40 +1100

    load woof env; conda not activating properly

---
+++ load_woof.sh
@@ -0,0 +1,4 @@
+ENV_NAME="woof"
+unset PYTHONPATH
+export PATH=/g/data3/gx8/projects/Diakumis/miniconda/envs/${ENV_NAME}/bin:/g/data3/gx8/projects/Diakumis/miniconda/bin:$PATH
+export CONDA_PREFIX=/g/data3/gx8/projects/Diakumis/miniconda/envs/${ENV_NAME}
