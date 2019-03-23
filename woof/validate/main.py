c7360aa78d3b65e0ef29ead5bd07aa0e70209176
pdiakumis <pdiakumis@unimelb.edu.au>
2019-03-23 20:31:25 +1100

    validate output structure

--- woof/validate/main.py
+++ woof/validate/main.py
@@ -1,16 +1,18 @@
 import os
-from os.path import isfile, join, dirname, abspath
 import sys
 import subprocess
 import click
-from click import echo
+from click import echo, style
+from woof import utils
+from woof.cromwell import run
 
 @click.command()
-@click.argument("input", type=click.File(), metavar="<input>")
-@click.option('--hpc', type=click.Choice(['local', 'spartan', 'raijin', 'aws']),
-              required=False, help="HPC system used")
-def validate(input, hpc):
-    """Validate NGS data"""
-    echo(click.style("In validate.main", fg='blue'))
-    echo(f'input is {input}; hpc is {hpc}')
+@click.argument("dirpath", type=click.File(), metavar="<dirpath>")
+def validate(dirpath):
+    """Validate NGS data in <dir>"""
 
+    echo(click.style("In validate.main", fg='blue'))
+
+    dirpath = utils.adjust_path(dirpath)
+
+    echo(f'<dirpath> is {dirpath}')
