e3f9f093005280ddb40c68227f5da30b97de8113
pdiakumis <peterdiakumis@gmail.com>
2022-10-12 00:30:49 +1100

    pin cromwell v48; drop rock

--- scripts/load_woof.sh
+++ scripts/load_woof.sh
@@ -1,4 +1,4 @@
 ENV_NAME="woof"
 unset PYTHONPATH
-export PATH=/g/data3/gx8/projects/Diakumis/miniconda/envs/${ENV_NAME}/bin:/g/data3/gx8/projects/Diakumis/miniconda/bin:$PATH
-export CONDA_PREFIX=/g/data3/gx8/projects/Diakumis/miniconda/envs/${ENV_NAME}
+export PATH=/Users/pdiakumis/conda/envs/${ENV_NAME}/bin:/Users/pdiakumis/conda/bin:$PATH
+export CONDA_PREFIX=/Users/pdiakumis/conda/envs/${ENV_NAME}
